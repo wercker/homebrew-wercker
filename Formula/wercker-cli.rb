3d28c039b205f4d7f3651c97ce068cded42f8756
mies <mies@miess-MacBook-Pro.local>
2015-12-14 16:24:40 +0100

    basic description

---
+++ Formula/wercker-cli.rb
@@ -0,0 +1,13 @@
+require "formula"
+
+class WerckerCLI < Formula
+  desc "wercker command line interface for building and running containers"
+  homepage "http://wercker.com"
+  url "https://s3.amazonaws.com/downloads.wercker.com/cli/stable/darwin_amd64/wercker"
+  sha1 ""
+  version ""
+
+  def install
+    bin.install Dir["*"]
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # homebrew-wercker
-Homebrew tap for the wercker command line interface
+
+Homebrew tap for the [wercker command line interface](http://wercker.com/cli)
+
+## Usage
+
+```
+brew tap wercker/wercker
+brew install wercker-cli
+```
