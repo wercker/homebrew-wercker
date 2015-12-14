a1160dca2d25d144188d01bccbe362651752ce64
Micha Hernandez van Leuffen <mies@wercker.com>
2015-12-14 17:26:04 +0100

    Update and rename wercker-client.rb to wercker-cli.rb

--- Formula/wercker-cli.rb
+++ Formula/wercker-cli.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class WerckerClient < Formula
+class WerckerCli < Formula
   desc "wercker command line interface for building and running containers"
   homepage "http://wercker.com"
   url "https://s3.amazonaws.com/downloads.wercker.com/cli/stable/darwin_amd64/wercker"
