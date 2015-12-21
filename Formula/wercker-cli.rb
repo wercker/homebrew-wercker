d7ea4ad9af9e736830b417812389daeb17b04ce4
wercker build <walter@wercker.com>
2015-12-21 18:21:50 +0000

    $VERSION

---
+++ Formula/wercker-cli.rb
@@ -0,0 +1,13 @@
+require "formula"
+
+class WerckerCli < Formula
+  desc "wercker command line interface for building and running containers"
+  homepage "http://wercker.com"
+  url "https://s3.amazonaws.com/downloads.wercker.com/cli/stable/darwin_amd64/wercker"
+  sha256 ""
+  version ""
+
+  def install
+    bin.install Dir["*"]
+  end
+end
