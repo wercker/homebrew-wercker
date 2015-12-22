cce219775ae46ff6ea5df484f65cf0d7ede32a75
wercker build <walter@wercker.com>
2015-12-22 11:53:00 +0000

    Release 1.2.3

--- Formula/wercker-cli.rb
+++ Formula/wercker-cli.rb
@@ -5,7 +5,7 @@ class WerckerCli < Formula
   homepage "http://wercker.com"
   url "https://s3.amazonaws.com/downloads.wercker.com/cli/stable/darwin_amd64/wercker"
   sha256 "aksjhdohdiasdk849u3ndfdsa"
-  version "3.4.x"
+  version "1.2.3"
 
   def install
     bin.install Dir["*"]
