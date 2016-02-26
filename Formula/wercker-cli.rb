9f072b79a8d3a0d416634b2edf0745363556c9bf
Benno van den Berg <bennovandenberg@gmail.com>
2016-02-26 11:37:59 +0100

    Update wercker-cli.rb

--- Formula/wercker-cli.rb
+++ Formula/wercker-cli.rb
@@ -3,9 +3,9 @@ require "formula"
 class WerckerCli < Formula
   desc "wercker command line interface for building and running containers"
   homepage "http://wercker.com"
-  url "https://s3.amazonaws.com/downloads.wercker.com/cli/versions/1.0.295/darwin_amd64/wercker"
-  sha256 "d7b3faddd00313fa023da3cb0150832b6e698f1050d7c25dcda52fb8b67a86a5"
-  version "1.0.295"
+  url "https://s3.amazonaws.com/downloads.wercker.com/cli/versions/1.0.405/darwin_amd64/wercker"
+  sha256 "73f3e2a561f2b01265f65a54dcd6f33e8bd8b1101b005aa31454129dff7297bf"
+  version "1.0.405"
 
   def install
     bin.install Dir["*"]
