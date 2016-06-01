323dfc3516eb582a8863ef2b851be4f528872889
Benno van den Berg <bennovandenberg@gmail.com>
2016-06-01 10:07:32 +0200

    Include version in the url (#7)

--- builder.sh
+++ builder.sh
@@ -7,7 +7,7 @@ require "formula"
 class WerckerCli < Formula
   desc "wercker command line interface for building and running containers"
   homepage "http://wercker.com"
-  url "https://s3.amazonaws.com/downloads.wercker.com/cli/stable/darwin_amd64/wercker"
+  url "https://s3.amazonaws.com/downloads.wercker.com/cli/versions/$VERSION/darwin_amd64/wercker"
   sha256 "$SHA256"
   version "$VERSION"
 
