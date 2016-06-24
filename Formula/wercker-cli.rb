fecd4b091fa96e59086efb96e1b6f0193f409f1b
Benno van den Berg <bennovandenberg@gmail.com>
2016-06-24 15:44:58 +0200

    Refactor to release-stable, prepare for dev channel

--- Formula/wercker-cli.rb
+++ Formula/wercker-cli.rb
@@ -3,9 +3,9 @@ require "formula"
 class WerckerCli < Formula
   desc "wercker command line interface for building and running containers"
   homepage "http://wercker.com"
-  url "https://s3.amazonaws.com/downloads.wercker.com/cli/versions/1.0.405/darwin_amd64/wercker"
-  sha256 "73f3e2a561f2b01265f65a54dcd6f33e8bd8b1101b005aa31454129dff7297bf"
-  version "1.0.405"
+  url ""
+  sha256 ""
+  version ""
 
   def install
     bin.install Dir["*"]
