00cbe18fb0054233746533c37138b9076747f14d
walter <walterbot@wercker.com>
2016-09-22 17:20:54 +0000

    Release beta 1.0.630-beta

--- Formula/wercker-cli.rb
+++ Formula/wercker-cli.rb
@@ -9,9 +9,9 @@ class WerckerCli < Formula
   version "1.0.629"
 
   devel do
-    url "https://s3.amazonaws.com/downloads.wercker.com/cli/versions/1.0.629-beta/darwin_amd64/wercker"
-    sha256 "be63bf8f7358006a69669e7c87cc587f5c5d6d662affc2528258f2f96a632aa3"
-    version "1.0.629-beta"
+    url "https://s3.amazonaws.com/downloads.wercker.com/cli/versions/1.0.630-beta/darwin_amd64/wercker"
+    sha256 "63b37ac62da0fb7625dcc24fab9430b2d867e38c26da9c9699514e1696d36136"
+    version "1.0.630-beta"
   end
 
   def install
