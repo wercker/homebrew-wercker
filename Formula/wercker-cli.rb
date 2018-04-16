c66803e6e6550946014a07ea9d68218486edab03
walter <walterbot@wercker.com>
2018-04-16 07:05:19 +0000

    Release stable 1.0.1201

--- Formula/wercker-cli.rb
+++ Formula/wercker-cli.rb
@@ -4,9 +4,9 @@ class WerckerCli < Formula
   desc "wercker command line interface for building and running containers"
   homepage "http://wercker.com"
 
-  url "https://s3.amazonaws.com/downloads.wercker.com/cli/versions/1.0.1197/darwin_amd64/wercker"
-  sha256 "03783faea7ee205734d2570b50b3e3e27444822696e11a411e65aa46b2f4af1a"
-  version "1.0.1197"
+  url "https://s3.amazonaws.com/downloads.wercker.com/cli/versions/1.0.1201/darwin_amd64/wercker"
+  sha256 "4722099103729118962d0b7c1b845a329c42b0545700d1194decb3451a8b9797"
+  version "1.0.1201"
 
   devel do
     url "https://s3.amazonaws.com/downloads.wercker.com/cli/versions/1.0.630-beta/darwin_amd64/wercker"
