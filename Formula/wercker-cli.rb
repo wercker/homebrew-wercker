6451bf577ba7692e2e442951ab2e6c0797b591a5
walter <walterbot@wercker.com>
2018-12-06 09:11:54 +0000

    Release stable 1.0.1435

--- Formula/wercker-cli.rb
+++ Formula/wercker-cli.rb
@@ -2,11 +2,11 @@ class WerckerCli < Formula
   desc "Wercker command-line interface for building and running containers"
   homepage "http://wercker.com"
 
-  url "http://downloads.wercker.com/cli/versions/1.0.1432/darwin_amd64/wercker", :using => :nounzip
-  sha256 "3be70115a60bd2ff9fc190cffa535bd0177794eeef2f3d2f71e387f7d7f2666e"
+  url "http://downloads.wercker.com/cli/versions/1.0.1435/darwin_amd64/wercker", :using => :nounzip
+  sha256 "548065cc08683fdda86228f136dff8d6acb0a7ad0f86506d2002962c798b33b9"
 
   # Version information for quick reference
-  # version "1.0.1432"
+  # version "1.0.1435"
 
   def install
     bin.install "wercker"
